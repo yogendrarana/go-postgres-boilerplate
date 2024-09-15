7b66822c38cd8b997a9974d6ac3d80c317c54ad1
Yogendra Rana <yogendrarana.tech@gmail.com>
2024-09-15 11:23:14 +0545

    First commit

---
+++ internal/server/server.go
@@ -0,0 +1,44 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/joho/godotenv/autoload"
+
+	"how-to-server/internal/routers"
+)
+
+type Server struct {
+	port   int
+	router *gin.Engine
+}
+
+// NewServer initializes and returns an *http.Server with the Gin router
+func NewServer() *http.Server {
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err != nil {
+		port = 8000
+	}
+
+	// Initialize the server and database
+	srv := &Server{
+		port:   port,
+		router: routers.NewRouter(),
+	}
+
+	// Declare Server config
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", srv.port),
+		Handler:      srv.router,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	return server
+}
